c2f38f710094b0e65d28ee7bc7a25eef9cf47b2a
Pragmadim <pul.95@mail.ru>
2019-02-11 20:14:25 +0300

    0.1

---
+++ palindrom.php
@@ -0,0 +1,15 @@
+<?php
+mb_internal_encoding('utf-8');
+
+$text = "а роза упала на лапу Азора";
+$result = "палиндром";
+
+mb_strtolower($text);
+
+echo "$text";
+/**
+ * Created by PhpStorm.
+ * User: Pragmadim
+ * Date: 09.02.2019
+ * Time: 15:19
+ */
